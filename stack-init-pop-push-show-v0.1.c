cb0747b3717d9512c1a201163d4a7c574d4b1916
weeki <451167147@qq.com>
2016-02-17 08:23:57 +0800

    stack first version

---
+++ stack-init-pop-push-show-v0.1.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXSIZE 20
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
+
+typedef int ElemType;
+typedef int Status;
+
+typedef struct
+{
+    ElemType *base;
+    ElemType *top;
+    int stackSize;
+}sqStack;
+
+Status initStack(sqStack *s)
+{
+    s->base=(ElemType *)malloc(STACK_INIT_SIZE*sizeof(ElemType));
+    if(!s->base)
+    {
+        exit(0);
+    }
+    s->top=s->base;
+    s->stackSize=STACK_INIT_SIZE;
+    return OK;
+}
+
+Status Show_Stack(sqStack *s)
+{
+    ElemType *p,*q;
+    p=s->base;
+    printf("\n");
+    while(p!=s->top)
+    {
+        printf("%d\t",*p);
+        p++;
+    }
+    printf("\n");
+    return OK;
+}
+
+Status Push(sqStack *s,ElemType e)
+{
+    if(s->top-s->base >=s->stackSize)
+    {
+        s->base=(ElemType *)realloc(s->base,(s->stackSize+STACKINCREMENT)*sizeof(ElemType));
+        if(!s->base)
+            exit(0);
+
+        s->top=s->base+s->stackSize;
+        s->stackSize+=STACKINCREMENT;
+    }
+
+    *(s->top)=e;
+    s->top++;
+    return OK;
+}
+
+Status Pop(sqStack *s,ElemType *e)
+{
+    if(s->top==s->base)
+        return 0;
+    *e=*--(s->top);
+    return OK;
+}
+
+Status Init_Stack_Type(sqStack *s)
+{
+
+    int flag=1,temp;
+	 initStack(s);
+    while(flag)
+    {
+        printf("输入一个数：");
+        scanf("%d",&temp);
+        Push(s,temp);
+        printf("Are you sure to continue(1/0):");
+        scanf("%d",&flag);
+    }
+    return OK;
+}
+
+
+void main()
+{
+    sqStack *s;
+	int *e;
+	e=(int *)malloc(sizeof(int));
+	 s=(sqStack *)malloc(sizeof(sqStack));
+    Init_Stack_Type(s);
+    Show_Stack(s);
+	Push(s,9);
+	Show_Stack(s);
+	Pop(s,e);
+	Show_Stack(s);
+	free(e);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
